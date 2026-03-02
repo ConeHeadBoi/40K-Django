aac80a1a304e293154f38119ffb0794276a9eee5
Esteban <estebanmunevaraquite@gmail.com>
2026-03-03 10:13:12 +1300

    Added a filtering/search bar

--- warhammer_site/collection/views.py
+++ warhammer_site/collection/views.py
@@ -1,6 +1,6 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from .models import Miniature
-from django.shortcuts import get_object_or_404
 
 # Create your views here.
 # View for the home page of the collection
@@ -10,10 +10,24 @@ def home_view(request):
 # View for displaying the details of a specific miniature
 def miniatures_list_view(request):
     miniatures = Miniature.objects.all().order_by('-created_at')
+    query = request.GET.get('q')
+    faction = request.GET.get('faction')
+
+    if query:
+        miniatures = miniatures.filter(name__icontains=query)
+
+    if faction:
+        miniatures = miniatures.filter(faction__iexact=faction)
+
+    factions = Miniature.objects.values_list('faction', flat=True).distinct()
+
     return render(
         request,
         'collection/miniatures_list.html',
-        {'miniatures': miniatures}
+        {
+            'miniatures': miniatures,
+            'factions': factions,
+        }
     )
 
 # View for displaying the details of a specific miniature
