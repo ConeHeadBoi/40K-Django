dee4e8751ebca8c19d5481dacd2362c25f175f77
Esteban <estebanmunevaraquite@gmail.com>
2026-03-03 09:09:24 +1300

    Made a page to list mini's

--- warhammer_site/collection/views.py
+++ warhammer_site/collection/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Miniature
 
 # Create your views here.
 def home_view(request):
     return render(request, 'collection/home.html')
+
+def miniatures_list_view(request):
+    miniatures = Miniature.objects.all().order_by('-created_at')
+    return render(
+        request,
+        'collection/miniatures_list.html',
+        {'miniatures': miniatures}
+    )
 
 def about_view(request):
     return render(request, 'collection/about.html')
