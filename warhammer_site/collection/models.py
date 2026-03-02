e0a7c0d5f633508ce5d9b071c9100fa29b5e5ea7
Esteban <estebanmunevaraquite@gmail.com>
2026-03-03 09:53:28 +1300

    Added images to the details of the minis

--- warhammer_site/collection/models.py
+++ warhammer_site/collection/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Miniature(models.Model):
+    image = models.ImageField(upload_to='miniatures/', blank=True, null=True)
     name = models.CharField(max_length=100)
     faction = models.CharField(max_length=100)
     points = models.IntegerField()
